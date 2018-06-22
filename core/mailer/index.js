b93ff331e99779de931155a5eb6cb15e555afa8d
Vitalius Kuchalskis <qualphey@gmail.com>
2018-06-23 00:16:22 +0300

    0.2.11

--- core/mailer/index.js
+++ core/mailer/index.js
@@ -67,12 +67,15 @@ module.exports = class {
 
               this_class.transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
-                  return console.log(error);
+                  res.send({ err: error });
+                  return console.error(error);
+                } else {
+                  res.send({ success: "success"});
                 }
                 console.log('Message sent: %s', info.messageId);
                 // Preview only available when sending through an Ethereal account
                 console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
+              
                 // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
                 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
               });
